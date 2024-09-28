f9f6fb8b2ed5b3469bceedf82b316c3e4c1b24f6
hamza-fabtechsol <muhammad.hamza@fabtechsol.com>
2024-09-28 11:45:01 +0500

    f commit

---
+++ js/main.js
@@ -0,0 +1,4 @@
+document.getElementById("toggleButton").addEventListener("click", function() {
+    const navLinks = document.querySelector(".nav-links");
+    navLinks.classList.toggle("show");
+});
